d75a54ddf6febd81a7fe72e205bf4d8286c7f7e5
Jonathan Tinoco <tinocojona@outlook.com>
2024-02-21 12:12:40 -0600

    Add project files.

---
+++ Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Semaforo.Models;
+using System.Windows;
+
+namespace Semaforo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UserNameTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+            if (LoginDataIsValid(username.Trim(), password.Trim()))
+            {
+                if (username.Equals("Araneda") && password.Equals("uMTf$qINx24H"))
+                {
+                    User user = new User(username, password);
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña incorrectos", "Error de inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Asegurese de rellenar todos los campos", "Alerta de inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private bool LoginDataIsValid(string username, string password)
+        {
+            return !username.Equals("") && !password.Equals("");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿namespace Semaforo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public User(int id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+        }
+
+        public User(string username)
+        {
+            Username = username;
+        }
+    }
+}
