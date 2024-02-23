543f9e05316a0c214a4ec67bc81f45f710b86463
Jonathan Tinoco <tinocojona@outlook.com>
2024-02-23 14:18:50 -0600

    Algoritmo para leer excel automaticamente con libreria spreedsheetlight

--- LoadExcel.xaml.cs
+++ LoadExcel.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Win32;
+using Semaforo.Models;
+using SpreadsheetLight;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Semaforo
@@ -15,7 +19,7 @@ namespace Semaforo
             InitializeComponent();
         }
 
-        private void selectExcel_Click(object sender, RoutedEventArgs e)
+        private async void selectExcel_Click(object sender, RoutedEventArgs e)
         {
             try
             {
@@ -33,14 +37,17 @@ namespace Semaforo
                 };
                 if (openFileDialog.ShowDialog() == true)
                 {
-                    MessageBox.Show(openFileDialog.FileName);
+                    selectExcel.IsEnabled = false;
+                    StatusTB.Text = "Procesando excel";
+                    CaptionTB.Text = "Espera solo unos segundos";
+                    List<Item> itemsCollection = await GetItemsFromExcel(openFileDialog.FileName);
                 }
             }
-            catch (UnauthorizedAccessException ex)
+            catch (UnauthorizedAccessException)
             {
                 MessageBox.Show("Sin permisos necesarios para acceder al archivo", "Error al cargar archivo excel", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            catch (FileNotFoundException ex)
+            catch (FileNotFoundException)
             {
                 MessageBox.Show("No se encontró el archivo", "Error al cargar archivo excel", MessageBoxButton.OK, MessageBoxImage.Error);
             }
@@ -55,6 +62,66 @@ namespace Semaforo
             Login login = new Login();
             Close();
             login.Show();
+        }
+
+        private async Task<List<Item>> GetItemsFromExcel(string path)
+        {
+            return await Task.Run(() =>
+            {
+                try
+                {
+                    List<Item> itemsCollection = new List<Item>();
+                    SLDocument excelFile = new SLDocument(path);
+                    SLWorksheetStatistics excelStatistics = excelFile.GetWorksheetStatistics();
+                    int endRowIndex = excelStatistics.EndRowIndex;
+                    string preItem = string.Empty;
+                    string subitem = string.Empty;
+                    for (int rowIndex = 6; rowIndex <= endRowIndex; rowIndex++)
+                    {
+                        string lotNumber = excelFile.GetCellValueAsString(rowIndex, 7);
+                        if (String.IsNullOrEmpty(lotNumber)) // Check if lot number is empty
+                        {
+                            if (!String.IsNullOrEmpty(excelFile.GetCellValueAsString(rowIndex, 3))) // Check if preitem is not empty
+                            {
+                                preItem = excelFile.GetCellValueAsString(rowIndex, 3);
+                                if (preItem.StartsWith("Total"))
+                                {
+                                    preItem = string.Empty;
+                                }
+                            }
+                            else // Get SubItem
+                            {
+                                subitem = excelFile.GetCellValueAsString(rowIndex, 4);
+                                if (subitem.StartsWith("Total"))
+                                {
+                                    subitem = string.Empty;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            string fullItem = String.IsNullOrEmpty(subitem) ? preItem : preItem + " " + subitem;
+                            DateTime expirationDate = excelFile.GetCellValueAsDateTime(rowIndex, 9);
+                            int daysUntilExpirationDate = excelFile.GetCellValueAsInt32(rowIndex, 11);
+                            double value = excelFile.GetCellValueAsDouble(rowIndex, 13);
+                            double quantity = excelFile.GetCellValueAsDouble(rowIndex, 15);
+                            Item newItem = new Item(fullItem, lotNumber, expirationDate, daysUntilExpirationDate, value, quantity);
+                            itemsCollection.Add(newItem);
+                        }
+                    }
+                    return itemsCollection;
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("El archivo seleccionado está siendo utilizado por otro programa", "Error al cargar archivo excel", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message}", "Error inesperado", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                return null;
+            });
+
         }
     }
 }
